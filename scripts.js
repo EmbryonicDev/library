0760aac4a92d75e065f358267592706788726879
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-03-02 14:36:30 +0100

    add code to enable form data to be captured  pushed to myLibrary

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 const addBtn = document.getElementById('addBtn');
+const form = document.getElementById('form');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const read = document.getElementById('read');
+
 let myLibrary = [];
+let newTitle = '';
+let newAuthor = '';
+let newPages = '';
+let newRead = '';
+let newBook = '';
 
 // Object constructor
 function book(title, author, pages, read) {
@@ -14,13 +25,45 @@ function book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  
+  newBook = new book(newTitle, newAuthor, newPages, newRead);
+  myLibrary.push(newBook)
 }
 
 addBtn.addEventListener('click', () => {
   console.log("clicked");
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  read.checked = false;
   const form = document.getElementById('form');
   form.style.visibility = "visible";
+})
+
+const formData = new FormData(document.querySelector('form'));
+console.log(formData);
+
+// function handleSubmit(e) {
+//   e.preventDefault();
+//   const formData = new FormData(e.target);
+//   const formProps = Object.fromEntries(formData);
+// }
+
+const check = (e) => {
+  const form = new FormData(e.target);
+  const title = form.get("title");
+  console.log(title);
+  return false
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.style.visibility = 'hidden';
+  newTitle = title.value;
+  newAuthor = author.value;
+  newPages = pages.value;
+  newRead = read.value;
+  addBookToLibrary();
+  console.log(myLibrary);
 })
 
 // Dummy Books
@@ -31,4 +74,4 @@ const book3 = new book('Psycho-Cybernetics', 'Maxwell Maltz', 336, true);
 myLibrary.push(book1);
 myLibrary.push(book2);
 myLibrary.push(book3);
-console.log(myLibrary)
+// console.log(myLibrary)
